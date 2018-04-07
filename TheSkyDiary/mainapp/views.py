c3a73592306c37213da32ca78cab4d2e03cf3f8d
sprread <sprread@gmail.com>
2018-04-07 14:00:45 -0700

    The see my sky button displays the email again

--- TheSkyDiary/mainapp/views.py
+++ TheSkyDiary/mainapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Request
+from . models import Request, Skies
 from django.core.mail import send_mail, EmailMessage
+from datetime import datetime
 
 
 # Create your views here.
@@ -9,9 +10,10 @@ from django.core.mail import send_mail, EmailMessage
 def index(request):
     return render(request, 'TheSkyDiary/index.html')
 
-#look at todolab
+
+
 def thankyou(request):
-    inquiry = Request()
+    inquiry = Request() #request is the class from my model
     sky_day = request.POST['sky-day']
     if int(sky_day) < 10:
         sky_day = '0' + sky_day
@@ -24,12 +26,17 @@ def thankyou(request):
     inquiry.email = request.POST['email']
     inquiry.save()
 
-    #check to see if I have that date, and if I don't, send email to myself.
-    # mail = EmailMessage(subject, text, ['EMAIL_ADDRESS'], [email])
-    # mail.attach(image1.name, attach.read(), attach.content_type)
-    # mail.send()
-    #look at Matthew's doc on email like the SMTP settings.
-
+    # check to see if I have that date, and if I don't, send email to myself.
+    if Skies.objects.filter(diary_date__year=sky_year,
+                      diary_date__month=sky_month,
+                      diary_date__day=sky_day).count() >= 1:
+        subject = "Your Sky Diary Proof"
+        text = "Please find your Sky Diary attached"
+        email = 'jackson.dh.reed@gmail.com'
+        mail = EmailMessage(subject, text, ['sprread@gmail.com'], [email])
+        sky = Skies.objects.get(diary_date__year=sky_year, diary_date__month=sky_month, diary_date__day=sky_day)
+        mail.attach(sky.watermarked_proof.url)
+        # mail.send()
     return render(request, 'TheSkyDiary/thankyou.html')
 
 
